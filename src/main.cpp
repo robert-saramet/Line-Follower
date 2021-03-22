7cafb305c309b429d72f6833de9c34abeebb1ae1
Robert Saramet <robert.crisrian@outlook.com>
2021-03-22 18:59:42 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,11 @@ float speedMultiplier = 1; //number to divide speed by
 
 L298NX2 robot(motorA_EN, motorA1, motorA2, motorB_EN, motorB1, motorB2); //initialize motor driver
 
+void steer(int speedA, int SpeedB){
+    robot.setSpeedA(speedA / speedMultiplier);
+    robot.setSpeedB(SpeedB /speedMultiplier);
+}
+
 //get value of each line sensor and store it in array
 void readLine(){
     for(int i = 1; i <= 5; i++){
@@ -38,95 +43,75 @@ void readLine(){
     }
 }
 
-void steer(int speedA, int SpeedB){
-    robot.setSpeedA(speedA / speedMultiplier);
-    robot.setSpeedB(SpeedB /speedMultiplier);
-}
-
 void followLine(){
     readLine();
     if (ls[1][1]==0 && ls[2][1]==0 && ls[3][1]==1 && ls[4][1]==0 && ls[5][1]==0){ 
-        //Serial.println("00100");
         robot.setSpeed(255 / speedMultiplier);
         robot.forward();
     }
     else if (ls[1][1]==0 && ls[2][1]==1 && ls[3][1]==1 && ls[4][1]==1 && ls[5][1]==0){ 
-        //Serial.println("01110");
         robot.setSpeed(255 / speedMultiplier);
         robot.forward();
     }
     else if (ls[1][1]==0 && ls[2][1]==1 && ls[3][1]==1 && ls[4][1]==0 && ls[5][1]==0){ 
-        //Serial.println("01100");
         steer(128, 255);
         robot.forwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==1 && ls[3][1]==0 && ls[4][1]==0 && ls[5][1]==0){ 
-        //Serial.println("01000");
         steer(64, 255);
         robot.forwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==1 && ls[2][1]==1 && ls[3][1]==0 && ls[4][1]==0 && ls[5][1]==0){ 
-        //Serial.println("11000");
         steer(128, 255);
         robot.backwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==1 && ls[2][1]==0 && ls[3][1]==0 && ls[4][1]==0 && ls[5][1]==0){ 
-        //Serial.println("10000");
         steer(255, 255);
         robot.backwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==0 && ls[3][1]==1 && ls[4][1]==1 && ls[5][1]==0){ 
-        //Serial.println("00110");
-        steer(255, 128);
         robot.forwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==0 && ls[3][1]==0 && ls[4][1]==1 && ls[5][1]==0){ 
-        //Serial.println("00010");
         steer(255, 64);
         robot.forwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==0 && ls[3][1]==0 && ls[4][1]==0 && ls[5][1]==1){ 
-        //Serial.println("00011");
         steer(255, 128);
         robot.forwardA();
         robot.backwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==0 && ls[3][1]==0 && ls[4][1]==0 && ls[5][1]==1){ 
-        //Serial.println("00001");
         steer(255, 255);
         robot.forwardA();
         robot.backwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==1 && ls[3][1]==1 && ls[4][1]==1 && ls[5][1]==0){ 
-        //Serial.println("01110");
         robot.setSpeed(255 / speedMultiplier);
         robot.forward();
     }
     else if (ls[1][1]==1 && ls[2][1]==1 && ls[3][1]==1 && ls[4][1]==0 && ls[5][1]==0){ 
-        //Serial.println("11100");
         steer(128, 255);
         robot.backwardA();
         robot.forwardB();
     }
     else if (ls[1][1]==0 && ls[2][1]==0 && ls[3][1]==1 && ls[4][1]==1 && ls[5][1]==1){ 
-        //Serial.println("00111");
         steer(255, 128);
         robot.forwardA();
         robot.backwardB();
     }
     else{
-      //Serial.println("ERROR");
     }
 }
 
 void setup(){
-    //Serial.begin(9600);
+    Serial.begin(9600);
     robot.setSpeed(255);
     robot.stop();
 
